97af6ce885eed07e4730256680fd8659e587e071
zhangtao <553873475@qq.com>
2016-11-24 21:15:51 +0800

    修改了MainActivity注释

--- app/src/main/java/com/example/administrator/demotest2/MainActivity.java
+++ app/src/main/java/com/example/administrator/demotest2/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.administrator.demotest2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//听说明天天气很冷的
 public class MainActivity extends AppCompatActivity {
 
     @Override
